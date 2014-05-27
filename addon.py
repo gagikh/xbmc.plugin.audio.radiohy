cb131831a54774896f39ac329f7a49180f338e85
Gagik <hakgagik@yahoo.com>
2014-05-27 23:32:38 +0400

    - added mail, web-page and country info.

--- addon.py
+++ addon.py
@@ -38,81 +38,137 @@ plugin.register_module(playlist, url_prefix='/_playlist')
 
 
 def get_streams():
+    #{
+    #    "Name":"HayFM",
+    #    "Url":"http://hayfm.am:8000/",
+    #    "Icon":""
+    #},
     resp = [
             {
-                "Name":"Ar Radio Intercontinental",
-                "URLStream":"http://199.195.194.92:8029/",
-                "Icon":"http://www.arradio.am/images/m_01.gif"
+                "Name":     "Ar Radio Intercontinental",
+                "Url":      "http://199.195.194.92:8029",
+                "Icon":     "http://www.arradio.am/images/m_01.gif",
+                "Email":    "aa@arradio.am",
+                "Country":  "Armenia",
+                "Phone":    "+374-10-55-11-43",
+                "WebPage":  "http://www.arradio.am"
             },
             {
-                "Name":"Armenian Patriotic Radio",
-                "URLStream":"http://5.35.246.210:8001/stream",
-                "Icon":"http://www.imarmenian.com/association2/wp-content/uploads/2011/12/ARF-Logo-Red.png"
+                "Name":     "Armenian Patriotic Radio",
+                "Url":      "http://5.35.246.210:8001/stream",
+                "Icon":     "http://www.imarmenian.com/association2/wp-content/uploads/2011/12/ARF-Logo-Red.png",
+                "Email":    "radioyan@imarmenian.com",
+                "Country":  "France",
+                "Phone":    "+961-3-274-847",
+                "WebPage":  "http://radioyan.com/"
             },
             {
-                "Name":"Armenian Pulse Radio",
-                "URLStream":"http://50.7.96.210:8134/",
-                "Icon":"http://www.armenianpulse.com/wp-content/themes/eGamer/images/radiopage/pulse_radio.jpg"
+                "Name":     "Armenian Pulse Radio",
+                "Url":      "http://50.7.96.210:8134/",
+                "Icon":     "http://www.armenianpulse.com/wp-content/themes/eGamer/images/radiopage/pulse_radio.jpg"
+                "Email":    "content@armenianpulse.com",
+                "Country":  "",
+                "Phone":    "",
+                "WebPage":  "http://www.armenianpulse.com/"
             },
             {
-                "Name":"Lav Radio(FM-107)",
-                "URLStream":"http://streams4.museter.com:8218/",
-                "Icon":"http://www.fm107.am/images/logo.jpg"
+                "Name":     "Lav Radio(FM-107)",
+                "Url":      "http://streams4.museter.com:8218/",
+                "Icon":     "http://www.fm107.am/images/logo.jpg"
+                "Email":    "",
+                "Country":  "Armenia",
+                "Phone":    "+374-10-36-86-45",
+                "WebPage":  "http://www.fm107.am/"
             },
             {
-                "Name":"HayFM",
-                "URLStream":"http://hayfm.am:8000/",
-                "Icon":""
+                "Name":     "Nor Radyo",
+                "Url":      "http://norradyo.com:8000/live",
+                "Icon":     "http://www.ermenikultur.org/wp-content/uploads/2013/10/Nor_Radyo__g_rsel.jpg"
+                "Email":    "",
+                "Country":  "Turkey",
+                "Phone":    "",
+                "WebPage":  "http://www.norradyo.com/"
             },
             {
-                "Name":"Nor Radyo",
-                "URLStream":"http://norradyo.com:8000/live",
-                "Icon":"http://www.ermenikultur.org/wp-content/uploads/2013/10/Nor_Radyo__g_rsel.jpg"
+                "Name":     "Radio Armenie",
+                "Url":      "http://radioarmenie.relay-network.com:8032/",
+                "Icon":     "http://www.radioarmenie.com/templates/theme475/images/logo.gif",
+                "Email":    "",
+                "Country":  "France",
+                "Phone":    "04-78-49-52-74",
+                "WebPage":  "http://www.radioarmenie.com/"
             },
             {
-                "Name":"Radio Armenie",
-                "URLStream":"http://radioarmenie.relay-network.com:8032/",
-                "Icon":"http://www.radioarmenie.com/templates/theme475/images/logo.gif"
+                "Name":     "Radio AVOL",
+                "Url":      "http://64.150.176.192:8250/stream",
+                "Icon":     "http://radioavol.org/uploads/donates/donate1x1.png",
+                "Email":    "info@radioavol.org",
+                "Country":  "Lebanon",
+                "Phone":    "",
+                "WebPage":  "http://radioavol.org/"
             },
             {
-                "Name":"Radio AVOL",
-                "URLStream":"http://64.150.176.192:8250/stream",
-                "Icon":"http://radioavol.org/uploads/donates/donate1x1.png"
+                "Name":     "Radio AYP",
+                "Url":      "http://stric6.streamakaci.com/radioayp.mp3",
+                "Icon":     "http://radio-aypfm.com/images/bientot_en_direct.jpg",
+                "Email":    "",
+                "Country":  "France",
+                "Phone":    "01-43-53-19-90",
+                "WebPage":  "http://radio-aypfm.com/"
             },
             {
-                "Name":"Radio AYP",
-                "URLStream":"http://stric6.streamakaci.com/radioayp.mp3",
-                "Icon":"http://radio-aypfm.com/images/bientot_en_direct.jpg"
+                "Name":     "Radio Hay",
+                "Url":      "http://mix.am:8000/rhyerevan",
+                "Icon":     "",
+                "Email":    "",
+                "Country":  "Armenia",
+                "Phone":    "",
+                "WebPage":  "http://radiohay.am/"
             },
             {
-                "Name":"Radio Hay",
-                "URLStream":"http://mix.am:8000/rhyerevan",
-                "Icon":""
+                "Name":     "Radio Jan",
+                "Url":      "http://streams4.museter.com:8216/",
+                "Icon":     "http://www.arm-radio.com/wp-content/uploads/2014/02/radio-jan-logo.png",
+                "Email":    "info@radiojan.am",
+                "Country":  "Armenia",
+                "Phone":    "+374-96-01-08-55",
+                "WebPage":  "http://www.radiojan.am/"
             },
             {
-                "Name":"Radio Jan",
-                "URLStream":"http://streams4.museter.com:8216/",
-                "Icon":"http://www.arm-radio.com/wp-content/uploads/2014/02/radio-jan-logo.png"
+                "Name":     "Radio Sevan",
+                "Url":      "http://sevan.bitwize.me:8018/",
+                "Icon":     "http://cdn9.staztic.com/app/a/991/991430/radio-sevan-live-1-0-s-156x156.jpg",
+                "Email":    "",
+                "Country":  "Beirut",
+                "Phone":    "+961-1-567161/2/3",
+                "WebPage":  "http://www.radiosevan.com/"
             },
             {
-                "Name":"Radio Sevan",
-                "URLStream":"http://sevan.bitwize.me:8018/",
-                "Icon":"http://cdn9.staztic.com/app/a/991/991430/radio-sevan-live-1-0-s-156x156.jpg"
+                "Name":     "Radio YAN ARMENIAN",
+                "Url":      "http://5.35.246.210:8000/stream",
+                "Icon":     "http://www.imarmenian.com/association2/wp-content/uploads/2011/11/radioyan-300x97.jpg",
+                "Email":    "radioyan@imarmenian.com",
+                "Country":  "France",
+                "Phone":    "+961-3-274-847",
+                "WebPage":  "http://www.radioyan.com/"
             },
             {
-                "Name":"Radio YAN ARMENIAN",
-                "URLStream":"http://5.35.246.210:8000/stream",
-                "Icon":"http://www.imarmenian.com/association2/wp-content/uploads/2011/11/radioyan-300x97.jpg"
+                "Name":     "Radio YAN FOLK",
+                "Url":      "http://5.35.246.210:8002/stream",
+                "Icon":     "http://old.hooys.com/banasdeghzootyoon/55_P20_Dare_Zadooroghli_a.jpg",
+                "Email":    "radioyan@imarmenian.com",
+                "Country":  "France",
+                "Phone":    "+961-3-274-847",
+                "WebPage":  "http://www.radioyan.com/"
             },
             {
-                "Name":"Radio YAN FOLK",
-                "URLStream":"http://5.35.246.210:8002/stream",
-                "Icon":"http://old.hooys.com/banasdeghzootyoon/55_P20_Dare_Zadooroghli_a.jpg"
-            },
-            {
-                "Name":"Yerevan Nights",
-                "URLStream":"mms://radio.yerevannights.com/YerevanNights",
-                "Icon":"http://t2.gstatic.com/images?q=tbn:ANd9GcT_gI2q43Rgxs0Exa6JcZr9X3usmX5fGs2Do6_JDDmSrJ2sva3o"
+                "Name":     "Yerevan Nights",
+                "Url":      "mms://radio.yerevannights.com/YerevanNights",
+                "Icon":     "http://t2.gstatic.com/images?q=tbn:ANd9GcT_gI2q43Rgxs0Exa6JcZr9X3usmX5fGs2Do6_JDDmSrJ2sva3o",
+                "Email":    "info@yerevannights.com",
+                "Country":  "Armenia",
+                "Phone":    "",
+                "WebPage":  "http://www.yerevannights.com/"
             }
     ]
     return resp
@@ -129,21 +185,22 @@ def show_homepage():
                         'label': Station['Name'], 
                         'url': plugin.url_for(
                                 'startplay', 
-                                URLStream=Station['URLStream'], 
+                                Url=Station['Url'], 
                                 Name=Station['Name'], 
                                 Icon=Station['Icon']), 
                         'thumbnail':Station['Icon']})
 
     return plugin.add_items(items)
 
-@plugin.route('/live/<Name>/<URLStream>/<Icon>')
-def startplay(URLStream, Name, Icon):
-    rtmpurl = URLStream
+@plugin.route('/live/<Name>/<Url>/<Icon>')
+def startplay(Url, Name, Icon):
+    rtmpurl = Url
     Thumb = Icon
     li = xbmcgui.ListItem(Name, Name, Thumb, Thumb)
     li.setInfo('music', {'Title':Name})
     xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(rtmpurl, li)
-    # Return an empty list so we can test with plugin.crawl() and plugin.interactive()
+    # Return an empty list so we can test with plugin.crawl() 
+    # and plugin.interactive()
     return []
 
 if __name__ == '__main__': 
