a67359554c6e749df23a0d562bf6b6a81cf1a52b
Gagik <hakgagik@yahoo.com>
2014-06-03 22:36:25 +0400

    - address and country info

--- resources/lib/stations.py
+++ resources/lib/stations.py
@@ -8,13 +8,15 @@ def getStations():
     #    "Icon":""
     #},
     resp = [
-            {
+            { # verified
                 "Name":     "Ar Radio Intercontinental",
                 "Url":      "http://199.195.194.92:8029",
                 "Icon":     "http://www.arradio.am/images/m_01.gif",
                 "Email":    "aa@arradio.am",
                 "Country":  "Armenia",
+                "Address":  "0025 Yerevan, Alex Manoogian 5 str",
                 "Phone":    "+374-10-55-11-43",
+                "Director": "Armen Amiryan"
                 "WebPage":  "http://www.arradio.am"
             },
             {
@@ -22,8 +24,10 @@ def getStations():
                 "Url":      "http://fire.wavestreamer.com:5643",
                 "Icon":     "http://bashde.org/images/logo-4e.png",
                 "Email":    "info@bashde.org",
-                "Country":  "",
+                "Country":  "USA",
+                "Address":  "405 Waltham St. Lexington, MA 02421-7954",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://bashde.org/"
             },
             {
@@ -32,7 +36,9 @@ def getStations():
                 "Icon":     "http://www.imarmenian.com/association2/wp-content/uploads/2011/12/ARF-Logo-Red.png",
                 "Email":    "radioyan@imarmenian.com",
                 "Country":  "France",
+                "Address":  "Maraash Street, Harboyan Building, 1203 Bourj Hammoud, Al-Metn, Lebanon",
                 "Phone":    "+961-3-274-847",
+                "Director": "",
                 "WebPage":  "http://radioyan.com/"
             },
             {
@@ -41,7 +47,9 @@ def getStations():
                 "Icon":     "http://www.armenianpulse.com/wp-content/themes/eGamer/images/radiopage/pulse_radio.jpg",
                 "Email":    "content@armenianpulse.com",
                 "Country":  "",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "Harout Kalandjian",
                 "WebPage":  "http://www.armenianpulse.com/"
             },
             {
@@ -50,16 +58,20 @@ def getStations():
                 "Icon":     "http://www.fm107.am/images/logo.jpg",
                 "Email":    "",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "+374-10-36-86-45",
+                "Director": "",
                 "WebPage":  "http://www.fm107.am/"
             },
             {
                 "Name":     "Nor Radyo",
                 "Url":      "http://norradyo.com:8000/live",
                 "Icon":     "http://www.ermenikultur.org/wp-content/uploads/2013/10/Nor_Radyo__g_rsel.jpg",
-                "Email":    "",
+                "Email":    "info@norradyo.com",
                 "Country":  "Turkey",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://www.norradyo.com/"
             },
             {
@@ -68,25 +80,31 @@ def getStations():
                 "Icon":     "http://votreradiosurlenet.com/player/img/radioa.gif",
                 "Email":    "radio.a@wanadoo.fr",
                 "Country":  "France",
+                "Address":  "32 rue Pompery, 26500 Bourg-les-Valence",
                 "Phone":    "00-33-4-75-56-18-33",
+                "Director": "",
                 "WebPage":  "http://radioa.net/"
             },
             {
-                "Name":     "Radio Aga",
+                "Name":     "Armenian Voice",
                 "Url":      "http://50.7.77.114:8111/autodj",
                 "Icon":     "http://www.armenianvoice.com/SiteImages/ArmenianVoice.jpg",
                 "Email":    "info@Armenianvoice.com",
                 "Country":  "",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://www.armenianvoice.com/"
             },
             {
                 "Name":     "Radio Arax",
-                "Url":      "http://radioarmenie.relay-network.com:8032/",
+                "Url":      "",
                 "Icon":     "http://www.radioarax.com/logo.jpg",
                 "Email":    "armenia@adinet.com.uy",
                 "Country":  "Uruguay",
+                "Address":  "",
                 "Phone":    "+598-90-09-69-09",
+                "Director": "",
                 "WebPage":  "http://www.radioarax.com/"
             },
             {
@@ -95,7 +113,9 @@ def getStations():
                 "Icon":     "http://www.radioarc.com/images/logo-anim.gif",
                 "Email":    "info@radioarc.com",
                 "Country":  "",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://www.radioarc.com/"
             },
             {
@@ -104,7 +124,9 @@ def getStations():
                 "Icon":     "http://www.radioarmenie.com/templates/theme475/images/logo.gif",
                 "Email":    "",
                 "Country":  "France",
+                "Address":  "",
                 "Phone":    "04-78-49-52-74",
+                "Director": "",
                 "WebPage":  "http://www.radioarmenie.com/"
             },
             {
@@ -113,7 +135,9 @@ def getStations():
                 "Icon":     "http://radioavol.org/uploads/donates/donate1x1.png",
                 "Email":    "info@radioavol.org",
                 "Country":  "Lebanon",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://radioavol.org/"
             },
             {
@@ -122,7 +146,9 @@ def getStations():
                 "Icon":     "http://www.radioaurora.am/images/white_logo.png",
                 "Email":    "radioaurora@radioaurora.am",
                 "Country":  "Armenia",
+                "Address":  "Ереван, улица Наири Зарьяна 22, этажи 7,9, 0051",
                 "Phone":    "+374-10-25-10-07",
+                "Director": "",
                 "WebPage":  "http://www.radioaurora.am/"
             },
             {
@@ -131,17 +157,76 @@ def getStations():
                 "Icon":     "http://radio-aypfm.com/images/bientot_en_direct.jpg",
                 "Email":    "",
                 "Country":  "France",
+                "Address":  "41 rue des écoles, 94140 Alfortville",
                 "Phone":    "01-43-53-19-90",
+                "Director": "",
                 "WebPage":  "http://radio-aypfm.com/"
             },
             {
-                "Name":     "NOP",
+                "Name":     "Armenian Independent Broadcasting of Boston",
                 "Url":      "http://ws.irib.ir/player/mediaplayer/player.swf",
-                "Icon":     "NOP",
-                "Email":    "NOP",
-                "Country":  "NOP",
-                "Phone":    "NOP",
+                "Icon":     "http://armenianradioboston.com/images/ABB-Bannerad.jpg",
+                "Email":    "Yevgine@aol.com",
+                "Country":  "USA",
+                "Address":  "PO Box 46, Watertown, Massachusetts 02471",
+                "Phone":    "+1-617-926-6268",
+                "Director": "Yevgine Gharibian",
                 "WebPage":  "http://armenianradioboston.com/"
+            },
+            {
+                "Name":     "Radio Hay (starfm)",
+                "Url":      "http://mix.am:8000/starfm",
+                "Icon":     "http://live.mix.am/templates/live/img/2.png",
+                "Email":    "",
+                "Country":  "Armenia",
+                "Address":  "",
+                "Phone":    "",
+                "Director": "",
+                "WebPage":  "http://live.mix.am/"
+            },
+            {
+                "Name":     "Radio Hay (mixfm)",
+                "Url":      "http://mix.am:8000/mixfm",
+                "Icon":     "http://live.mix.am/templates/live/img/3.png",
+                "Email":    "",
+                "Country":  "Armenia",
+                "Address":  "",
+                "Phone":    "",
+                "Director": "",
+                "WebPage":  "http://live.mix.am/"
+            },
+            {
+                "Name":     "Radio Hay (Hay FM)",
+                "Url":      "http://mix.am:8000/radiohyfm",
+                "Icon":     "http://live.mix.am/templates/live/img/4.png",
+                "Email":    "",
+                "Country":  "Armenia",
+                "Address":  "",
+                "Phone":    "",
+                "Director": "",
+                "WebPage":  "http://live.mix.am/"
+            },
+            {
+                "Name":     "Radio Hay (Anr)",
+                "Url":      "http://mix.am:8000/anr24",
+                "Icon":     "http://live.mix.am/templates/live/img/5.png",
+                "Email":    "",
+                "Country":  "Arcax",
+                "Address":  "",
+                "Phone":    "",
+                "Director": "",
+                "WebPage":  "http://live.mix.am/"
+            },
+            {
+                "Name":     "Radio Hay (Smile)",
+                "Url":      "http://mix.am:8000/smile",
+                "Icon":     "http://live.mix.am/templates/live/img/6.png",
+                "Email":    "",
+                "Country":  "Armenia",
+                "Address":  "",
+                "Phone":    "",
+                "Director": "",
+                "WebPage":  "http://live.mix.am/"
             },
             {
                 "Name":     "Radio Hay (5 achok)",
@@ -149,7 +234,9 @@ def getStations():
                 "Icon":     "",
                 "Email":    "",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://live.mix.am/"
             },
             {
@@ -158,7 +245,9 @@ def getStations():
                 "Icon":     "",
                 "Email":    "",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://radiohay.am/"
             },
             {
@@ -167,16 +256,20 @@ def getStations():
                 "Icon":     "http://www.arm-radio.com/wp-content/uploads/2014/02/radio-jan-logo.png",
                 "Email":    "info@radiojan.am",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "+374-96-01-08-55",
+                "Director": "",
                 "WebPage":  "http://www.radiojan.am/"
             },
             {
                 "Name":     "Radio Jazz",
-                "Url":      "NOP",
+                "Url":      "http://radiojazz.am/wp-content/themes/ari/mp3Player2.sw",
                 "Icon":     "http://radiojazz.am/wp-content/themes/ari/images/logo.png",
                 "Email":    "",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://radiojazz.am/"
             },
             {
@@ -185,7 +278,9 @@ def getStations():
                 "Icon":     "http://radiotoot.com/eng/wp-content/uploads/2013/06/logosmall32.png",
                 "Email":    "",
                 "Country":  "Armenia",
+                "Address":  "",
                 "Phone":    "",
+                "Address":  "",
                 "WebPage":  "http://radiotoot.com/"
             },
             {
@@ -193,8 +288,10 @@ def getStations():
                 "Url":      "http://sevan.bitwize.me:8018/",
                 "Icon":     "http://cdn9.staztic.com/app/a/991/991430/radio-sevan-live-1-0-s-156x156.jpg",
                 "Email":    "",
-                "Country":  "Beirut",
+                "Country":  "Lebanon",
+                "Address":  "Beirut, Khatchadurian Street, Khederlarian Building, Ground Floor",
                 "Phone":    "+961-1-567161/2/3",
+                "Director": "",
                 "WebPage":  "http://www.radiosevan.com/"
             },
             {
@@ -203,7 +300,9 @@ def getStations():
                 "Icon":     "http://www.radiovan.am/assets/pictures/logo.png",
                 "Email":    "radiovan@radiovan.am",
                 "Country":  "Armenia",
+                "Address":  "0100, Армения, Ереван, Ханджян 13а, 7ой этаж, Радио «Ван»",
                 "Phone":    "+374-10-54-00-01",
+                "Director": "",
                 "WebPage":  "http://radiovan.am"
             },
             {
@@ -212,7 +311,9 @@ def getStations():
                 "Icon":     "http://www.imarmenian.com/association2/wp-content/uploads/2011/11/radioyan-300x97.jpg",
                 "Email":    "radioyan@imarmenian.com",
                 "Country":  "France",
+                "Address":  "Maraash Street, Harboyan Building, 1203 Bourj Hammoud, Al-Metn, Lebanon",
                 "Phone":    "+961-3-274-847",
+                "Director": "",
                 "WebPage":  "http://www.radioyan.com/"
             },
             {
@@ -221,7 +322,9 @@ def getStations():
                 "Icon":     "http://old.hooys.com/banasdeghzootyoon/55_P20_Dare_Zadooroghli_a.jpg",
                 "Email":    "radioyan@imarmenian.com",
                 "Country":  "France",
+                "Address":  "Maraash Street, Harboyan Building, 1203 Bourj Hammoud, Al-Metn, Lebanon",
                 "Phone":    "+961-3-274-847",
+                "Director": "",
                 "WebPage":  "http://www.radioyan.com/"
             },
             {
@@ -229,8 +332,10 @@ def getStations():
                 "Url":      "http://50.7.173.162:8233/",
                 "Icon":     "http://radioyeraz.com/yeraz/wp-content/uploads/2013/12/radiologo.png",
                 "Email":    "radioyan@imarmenian.com",
-                "Country":  "Armenia",
+                "Country":  "",
+                "Address":  "",
                 "Phone":    "",
+                "Director": "",
                 "WebPage":  "http://radioyeraz.com/"
             },
             {
@@ -239,7 +344,9 @@ def getStations():
                 "Icon":     "http://www.voiceofvan.net/sites/all/themes/layout6/images/header-object.png",
                 "Email":    "",
                 "Country":  "Lebanon",
+                "Address":  "",
                 "Phone":    "+961-1-241-199 ",
+                "Director": "",
                 "WebPage":  "http://www.voiceofvan.net/"
             },
             {
@@ -247,8 +354,10 @@ def getStations():
                 "Url":      "mms://radio.yerevannights.com/YerevanNights",
                 "Icon":     "http://t2.gstatic.com/images?q=tbn:ANd9GcT_gI2q43Rgxs0Exa6JcZr9X3usmX5fGs2Do6_JDDmSrJ2sva3o",
                 "Email":    "info@yerevannights.com",
-                "Country":  "Armenia",
-                "Phone":    "",
+                "Country":  "USA",
+                "Address":  "3200 Wilshire Blvd. Ste 902NT, Los Angeles, CA, United States 90010",
+                "Phone":    "+1-877-220-8951",
+                "Director": "",
                 "WebPage":  "http://www.yerevannights.com/"
             }
     ]
