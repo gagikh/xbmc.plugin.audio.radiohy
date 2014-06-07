15a86a4618c38918f2dff6859aa537403c860227
djgago <hakgagik@yahoo.com>
2014-06-07 19:31:04 +0400

    Update stations.py
    - fixed radio ardzaganq

--- resources/lib/stations.py
+++ resources/lib/stations.py
@@ -156,7 +156,7 @@ def getStations(sortingKey):
             },
             {
                 "Name":     "Radio Ardzaganq",
-                "Url":      "http://www.ardzagank.com/home.swf",
+                "Url":      "http://de1.fananoon.com:7000",
                 "Icon":     "http://www.ardzagank.com/img/logo_3.jpg",
                 "Email":    "ardzagank@ardzagank.com",
                 "Country":  "Armenia",
